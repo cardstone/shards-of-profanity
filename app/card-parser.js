d1a207967e31cf224d001030b282d4635d6526f9
amaldare93 <amaldare93@gmail.com>
2015-09-10 10:42:20 +0200

    moved card-parser.js

---
+++ app/card-parser.js
@@ -0,0 +1,28 @@
+/**
+ * command line tool
+ * parses text files and outputs each card on a new line
+ *
+ * USE: node card-parser [file]
+ *
+ * TODO: write to database
+ **/
+
+var through = require('through2'),
+  fs = require('fs'),
+  split = require('split'),
+  readFile = process.argv[2],
+  writeFile = 'cards/output.txt';
+
+// transform stream
+var tr = through(function (buffer, _, next) {
+  this.push(buffer.toString() + '\n');
+  next();
+});
+
+//
+var write = fs.createWriteStream(writeFile);
+
+fs.createReadStream(readFile)
+  .pipe(split('<>'))
+  .pipe(tr)
+  .pipe(write);
