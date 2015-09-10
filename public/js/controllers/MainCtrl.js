617cae5a799dd1da39fa2cb924b177872e4f6388
Alvin Zablan <alvin.zablan62@myhunter.cuny.edu>
2015-09-09 20:00:10 -0400

    cleaned up mainctrl.js

--- public/js/controllers/MainCtrl.js
+++ public/js/controllers/MainCtrl.js
@@ -1,6 +1,3 @@
-//angular.module('MainCtrl', []).controller('MainController', function ($scope) {
 module.exports = function () {
-	var main = this;
-	main.tagline = 'Yooooooooo this is the main controller!!!';
-
+	this.tagline = 'Yooooooooo this is the main controller!!!';
 };
