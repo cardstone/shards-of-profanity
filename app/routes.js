81a4092f1d66258af3e45db3f65693b7a502c928
Alvin <alvin.zablan62@myhunter.cuny.edu>
2015-09-10 11:56:00 -0400

    fixed routes.js error

--- app/routes.js
+++ app/routes.js
@@ -7,7 +7,7 @@ module.exports = function (app) {
 	// frontend routes =========================================================
 	// route to handle all angular requests
 	app.get('*', function (req, res) {
-		res.sendFile(path.resolve('../../dist/index.html'));
+		res.sendFile(path.join(__dirname, '../dist','index.html'));
 	});
 
 };
