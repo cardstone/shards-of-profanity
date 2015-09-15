701d52e59441b944927438ec44e434cf8bc4fd94
andrewmilo <amiloslavsky@gmail.com>
2015-09-15 00:30:36 -0400

    Fixed db link

--- config/db.js
+++ config/db.js
@@ -1,4 +1,4 @@
 // config object for database
 module.exports = {
-  url: 'mongodb://cardstone:capstone@ec2-52-6-28-27.compute-1.amazonaws.com:27017'
+  url: 'mongodb://cardstone:capstone@ec2-52-6-28-27.compute-1.amazonaws.com:27017/admin'
 };
