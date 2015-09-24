0b32336dede94086221e360b57708b3d3aa9fbec
andrewmilo <amiloslavsky@gmail.com>
2015-09-24 17:17:01 -0400

    Updated card schema

--- app/models/Card.js
+++ app/models/Card.js
@@ -1,9 +1,18 @@
+/**
+ * Model File
+ * Creates a model from a custom Schema
+ *
+ **/
+
 var mongoose = require(mongoose),
   Schema = mongoose.Schema; 
 
+// Schema for cards
 var cardSchema = new Schema({
   color: String,
-  text: String
+  text: String,
+  pack: String
 });
 
+//Create the model from the Card schema and export it
 module.exports = mongoose.model('Card', cardSchema);
