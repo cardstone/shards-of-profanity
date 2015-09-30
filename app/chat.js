f42438ad5aadac90a4804e51725a492d934ddd2b
amaldare93 <amaldare93@gmail.com>
2015-09-30 18:41:57 -0400

    reformat chat messages with name, message, avatar

--- app/chat.js
+++ app/chat.js
@@ -11,16 +11,21 @@ exports.initChat = function (sio, socket, socketsInfo) {
 	gameSocket.on('client:joinSuccess', messagePlayerJoined);
 };
 
-function sendMessage (data) {
+function sendMessage(data) {
 	var gameNum = socketsObj[this.id].room;
 	var name = socketsObj[this.id].name;
-	var msg = name + ': ' + data.msg;
-	io.sockets.in(gameNum).emit('client:message', {msg: msg});
+	var msg = data.msg;
+	io.sockets.in(gameNum).emit('client:message', {
+		name: name,
+		msg: msg
+	});
 }
 
-function messagePlayerJoined (data) {
+function messagePlayerJoined(data) {
 	var gameNum = socketsObj[this.id].room;
 	var name = socketsObj[this.id].name;
 	var joinedMessage = name + ' joined the game.';
-	io.sockets.in(gameNum).emit('server:message', {msg: joinedMessage});
+	io.sockets.in(gameNum).emit('server:message', {
+		msg: joinedMessage
+	});
 }
