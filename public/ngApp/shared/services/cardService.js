383b846e5acd1cc1c7121e7d70bdf7be745c37cc
Alvin <alvin.zablan62@myhunter.cuny.edu>
2015-10-05 19:08:33 -0400

    start card service

---
+++ public/ngApp/shared/services/cardService.js
@@ -0,0 +1,23 @@
+(function () {
+	'use strict';
+
+	angular
+		.module('app')
+		.factory('CardService', [CardService]);
+
+	function CardService(){
+		// services are initialized only once.
+		// fill array with cards here, calling the script idk
+		// maybe an array for white and an array for black
+		// var white = []
+		// var black = [] 
+
+		// return {
+		// 	getWhite: function() {
+		// 		return white
+		// 	}
+		// 	getBlack:...
+		// };
+	}
+
+})();
