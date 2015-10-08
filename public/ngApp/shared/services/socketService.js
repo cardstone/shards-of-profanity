77cd6c56a57e1084dd4fe643256dd7ed845f9038
Alvin <alvin.zablan62@myhunter.cuny.edu>
2015-10-08 16:24:10 -0400

    switch socketService to use btford's angular-socket-io

--- public/ngApp/shared/services/socketService.js
+++ public/ngApp/shared/services/socketService.js
@@ -3,29 +3,34 @@
 
   angular
     .module('app')
-    .factory('SocketService', ['$rootScope', SocketService]);
+      .factory('SocketService', function (socketFactory) {
+        return socketFactory();
+      });
+  // angular
+  //   .module('app')
+  //   .factory('SocketService', ['$rootScope', SocketService]);
 
-  function SocketService($rootScope) {
-    var socket = io.connect();
-    return {
-      on: function (eventName, callback) {
-        socket.on(eventName, function () {
-          var args = arguments;
-          $rootScope.$apply(function () {
-            callback.apply(socket, args);
-          });
-        });
-      },
-      emit: function (eventName, data, callback) {
-        socket.emit(eventName, data, function () {
-          var args = arguments;
-          $rootScope.$apply(function () {
-            if (callback) {
-              callback.apply(socket, args);
-            }
-          });
-        });
-      }
-    };
-  }
+  // function SocketService($rootScope) {
+  //   var socket = io.connect();
+  //   return {
+  //     on: function (eventName, callback) {
+  //       socket.on(eventName, function () {
+  //         var args = arguments;
+  //         $rootScope.$apply(function () {
+  //           callback.apply(socket, args);
+  //         });
+  //       });
+  //     },
+  //     emit: function (eventName, data, callback) {
+  //       socket.emit(eventName, data, function () {
+  //         var args = arguments;
+  //         $rootScope.$apply(function () {
+  //           if (callback) {
+  //             callback.apply(socket, args);
+  //           }
+  //         });
+  //       });
+  //     }
+  //   };
+  // }
 })();
