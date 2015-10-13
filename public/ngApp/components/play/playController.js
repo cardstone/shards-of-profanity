fc4e995279add5439e9661797f9f69eb02f07d5b
Alvin <alvin.zablan62@myhunter.cuny.edu>
2015-10-13 16:13:04 -0400

    add 'hand' for white cards

--- public/ngApp/components/play/playController.js
+++ public/ngApp/components/play/playController.js
@@ -9,10 +9,12 @@
 		var play = this;
 		var socket = $scope.mySocket;
 		play.color = null;
+		$scope.hand = [];
 
 		socket.on('server:card', function (data) {
 			//console.log('got card');
 			$scope.card = data.card.text;
+			$scope.hand.push(data.card);
 		});
 
 		play.getRandWhite = function() {
