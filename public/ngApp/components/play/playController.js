27a659965f53f804780a00c0e98b9d7190602abb
Alvin Zablan <azablan.dev@gmail.com>
2015-10-18 13:29:11 +0100

    add max hand size of 10, drawing while having a full hand drops cards (queue)

--- public/ngApp/components/play/playController.js
+++ public/ngApp/components/play/playController.js
@@ -5,7 +5,7 @@
 		.module('app')
 		.controller('PlayController', ['$scope', PlayController]);
 
-	function PlayController($scope) {
+	function PlayController ($scope) {
 		var play = this;
 		var socket = $scope.mySocket;
 		$scope.hand = [];
@@ -17,16 +17,20 @@
 		});
 
 		socket.on('server:whiteCard', function (data) {
+			if($scope.hand.length == 10) {
+				var numNewCards = data.cards.length;
+				$scope.hand.splice(0, numNewCards);	
+			}
 			$scope.hand.push.apply($scope.hand, data.cards);
 		});
 
-		play.getRandWhite = function(numCards) {
+		play.getRandWhite = function (numCards) {
 			socket.emit('client:getRandWhite', {numCards: numCards});
-		}
+		};
 
-		play.getRandBlack = function() {
+		play.getRandBlack = function () {
 			socket.emit('client:getRandBlack');
-		}
+		};
 	}
 
 })();
