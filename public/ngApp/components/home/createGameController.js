956e02d3f0cf435ea008784114157226e1aa7f80
Alvin Zablan <azablan.dev@gmail.com>
2015-11-04 14:38:04 -0500

    add game name when creating game

--- public/ngApp/components/home/createGameController.js
+++ public/ngApp/components/home/createGameController.js
@@ -6,12 +6,14 @@
 		.controller('createGameController', ['$scope', '$state', '$stateParams', 'SocketService', createGameController]);
 
 	function createGameController($scope, $state, $stateParams, SocketService) {
+		$scope.gameName = null;
+
 		SocketService.on('server:createSuccess', function (data) {
 			$state.go('joinGame', {gameId: data.gameId, host: true});
 		});
 
 		$scope.createNewGame = function () {
-			SocketService.emit('client:createNewGame');
+			SocketService.emit('client:createNewGame', {gameName: $scope.gameName});
 		};
 
 	}
