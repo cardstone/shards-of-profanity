abe4777c270a8dfad52b742e69e0fc4d0ffbcd60
Alvin Zablan <azablan.dev@gmail.com>
2015-11-11 21:00:20 -0500

    edit create game menu

--- public/ngApp/components/home/createGameController.js
+++ public/ngApp/components/home/createGameController.js
@@ -7,37 +7,40 @@
 
 	function createGameController($scope, $state, $stateParams, SocketService) {
 		$scope.gameName = "shitty-default-name";
-	   	$scope.maxPlayers = {
-		    options: [
-			    {num: 3},
-			    {num: 4},
-			    {num: 5},
-			    {num: 6},
-			    {num: 7},
-			    {num: 8},
-			    {num: 9},
-			    {num: 10},
-		    ],
-    		selectedOption: {num: 3, name: '3 players'}
-    	};
-		$scope.privateMatch = {
-			options: [
-				{val: 0, name: 'public'},
-				{val: 1, name: 'private'},
-			],
-			selectedOption: {val: 0, name: 'public'}
-		};
-		$scope.maxPoints = {
-			options: [
-			    {num: 5},
-			    {num: 10},
-			    {num: 15},
-			    {num: 20},
-			    {num: 25},
-			    {num: 30},
-		    ],
-    		selectedOption: {num: 5}
-		}; 
+		$scope.maxPlayers = 3;
+		$scope.maxPoints = 5;
+		$scope.privateMatch = false;
+	 //   	$scope.maxPlayers = {
+		//     options: [
+		// 	    {num: 3},
+		// 	    {num: 4},
+		// 	    {num: 5},
+		// 	    {num: 6},
+		// 	    {num: 7},
+		// 	    {num: 8},
+		// 	    {num: 9},
+		// 	    {num: 10},
+		//     ],
+  //   		selectedOption: {num: 3, name: '3 players'}
+  //   	};
+		// $scope.privateMatch = {
+		// 	options: [
+		// 		{val: 0, name: 'public'},
+		// 		{val: 1, name: 'private'},
+		// 	],
+		// 	selectedOption: {val: 0, name: 'public'}
+		// };
+		// $scope.maxPoints = {
+		// 	options: [
+		// 	    {num: 5},
+		// 	    {num: 10},
+		// 	    {num: 15},
+		// 	    {num: 20},
+		// 	    {num: 25},
+		// 	    {num: 30},
+		//     ],
+  //   		selectedOption: {num: 5}
+		// }; 
 
 		SocketService.on('server:createSuccess', function (data) {
 			$state.go('joinGame', {
@@ -49,9 +52,9 @@
 		$scope.createNewGame = function () {
 			SocketService.emit('client:createNewGame', {
 				gameName: $scope.gameName,
-				maxPlayers: $scope.maxPlayers.selectedOption.num,
-				privateMatch: $scope.privateMatch.selectedOption.val,
-				maxPoints: $scope.maxPoints.selectedOption.num
+				maxPlayers: $scope.maxPlayers,
+				privateMatch: $scope.privateMatch,
+				maxPoints: $scope.maxPoints
 			});
 		};
 
