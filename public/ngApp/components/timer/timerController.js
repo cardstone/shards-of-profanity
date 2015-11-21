772b3470e26e506cd577d7bda0041f48eb021870
Alvin Zablan <azablan.dev@gmail.com>
2015-11-21 16:30:33 -0500

    move timer logic into controller, progress

--- public/ngApp/components/timer/timerController.js
+++ public/ngApp/components/timer/timerController.js
@@ -3,17 +3,23 @@
 
 	angular
 		.module('app')
-		.controller('TimerController', ['$scope', '$interval', TimerController]);
+		.controller('TimerController', [
+			'$scope',
+			'$interval',
+			'$timeout',
+			TimerController
+		]);
 
-	function TimerController($scope, $interval) {
+	function TimerController($scope, $interval, $timeout) {
 		var vm = this;
 		var socket = $scope.mySocket;
 		$scope.roundTime = 0;
 		$scope.czar = false;
-		vm.myStatus = "waiting for host to start";
+		$scope.myStatus = "waiting for host to start..";
 
 		socket.on('server:czar', function () {
 			$scope.czar = true;
+			$scope.myStatus = "";
 		});
 
 		socket.on('server:unCzar', function () {
@@ -23,7 +29,22 @@
 		socket.on('server:enableSubmit', function () {
 			$scope.roundTime = 25;
 			$interval(function(){$scope.roundTime--;}, 1000, 25);
+			$timeout(roundTimeUp, 25 * 1000);
+			if($scope.czar) {
+				$scope.myStatus = "waiting for players to submit...";
+			}
+			else {
+				$scope.myStatus = "submit your white cards.";
+			}
 		});
 
+		function roundTimeUp() {
+			if($scope.czar) {
+				$scope.myStatus = "select a winner.";
+			}
+			else {
+				$scope.myStatus = "awaiting the Czar's almighty ruling...";
+			}
+		}
 	}
 })();
